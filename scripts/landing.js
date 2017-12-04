c50b05b431cc6f8d0737a79d78517882d86c5897
Joseph <jvg_0119@yahoo.com>
2017-12-04 15:53:17 -0800

    assign 22 dom animantion

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,25 @@
 
 var animatePoints = function() {
-  var points = document.getElementsByClassName('point')
-  var revealFirstPoint = function() {
-    points[0].style.opacity = 1;
-    points[0].style.transform = "scaleX(1) translateY(0)";
-    points[0].style.msTransform = "scaleX(1) translateY(0)";
-    points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-  var revealSecondPoint = function() {
-    points[1].style.opacity = 1;
-    points[1].style.transform = "scaleX(1) translateY(0)";
-    points[1].style.msTransform = "scaleX(1) translateY(0)";
-    points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-  var revealThirdPoint = function() {
-    points[2].style.opacity = 1;
-    points[2].style.transform = "scaleX(1) translateY(0)";
-    points[2].style.msTransform = "scaleX(1) translateY(0)";
-    points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-  revealFirstPoint();
-  revealSecondPoint();
-  revealThirdPoint();
+  var points = document.getElementsByClassName('point');
+
+  // for (var i = 0; i < points.length; i++) {
+  //   // console.log(points[i]);
+  //   points[i].style.opacity = 1;
+  //   points[i].style.transform = "scaleX(1) translateY(0)";
+  //   points[i].style.msTransform = "scaleX(1) translateY(i)";
+  //   points[i].style.WebkitTransform = "scaleX(1) translaieY(0)";
+  // }
+
+  var revealPoint = function(i) {
+    points[i].style.opacity = 1;
+    points[i].style.transform = "scaleX(1) translateY(0)";
+    points[i].style.msTransform = "scaleX(1) translateY(i)";
+    points[i].style.WebkitTransform = "scaleX(1) translaieY(0)";
+  }
+
+  for (var i = 0; i < points.length; i++) {
+    revealPoint(i);
+  }
 }
-
-// setTimeout(function() {console.log("this is setTimeout")}, 1000);
+// animatePoints();
 // setTimeout(animatePoints, 1000);
-// https://developer.mozilla.org/en-US/docs/Web/CSS/transform
-
-//animatePoints();
