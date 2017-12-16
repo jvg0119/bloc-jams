bf82fac261e22467b5ffb6f72fa14075ac8ee8fa
Joseph <jvg_0119@yahoo.com>
2017-12-16 11:47:10 -0800

    cp 26 dom play pause 1

--- scripts/album.js
+++ scripts/album.js
@@ -41,7 +41,9 @@
 var createSongRow = function(songNumber, songName, songLength) {
   var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+  //    + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+                                        // added custom attribute to template
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -51,13 +53,10 @@ var createSongRow = function(songNumber, songName, songLength) {
 //console.log(createSongRow());
 
 
-
-
 var setCurrentAlbum = function(album) {
   // console.log('setCurrentAlbum is now running!')
 
   // these variables come from album.html file
-
   var albumTitle = document.getElementsByClassName('album-view-title')[0];
   var albumArtist = document.getElementsByClassName('album-view-artist')[0];
   var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
@@ -73,8 +72,6 @@ var setCurrentAlbum = function(album) {
   // console.log('albumImage= ',  albumImage);
   // albumSongList.textContent = "booooo"
   // console.log('albumSongList= ',  albumSongList);
-
-
 
 
   // these are for the album title:
@@ -121,11 +118,78 @@ var setCurrentAlbum = function(album) {
 }
 
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+//console.log(songListContainer);
+//var playButtonTemplate = "<a class=""><span class='ion-android-arrow-dropright-circle'></span></a>"
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// var songItemNumber = document.getElementsByClassName('song-item-number').childNodes;
+// console.log('songItemNumber = ',   songItemNumber);
+
 window.onload = function() {
   setCurrentAlbum(albumPicasso);
   // setCurrentAlbum(albumMarconi); // you can use the other data above
 
- };
+  songListContainer.addEventListener('mouseover', function(e) {
+
+    //console.log(e.target); // the node target; these are the single components
+    // console.log(e.target.parentNode); // parentNode is 'album-view-song-item'; this is the whole line
+    // console.log(e.target.parentNode.className = "album-view-song-item"); // this will change the class to whatever you assign it to
+
+    // if (e.target.textContent === '1') { // this is a quick manual test
+    //  console.log('this is number one')
+
+    //  e.target.innerHTML = playButtonTemplate // this will assign or change the target you mouseover to the playButtonTemplate
+
+//  if (e.target.className === "song-item-number") {  // this is triggered by the number only
+    if (e.target.parentElement.className === "album-view-song-item") {  // this is triggered by the line
+       e.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+       //console.log('booo = ',  e.target.parentElement.querySelector('.song-item-number').innerHTML)
+    }
+  });
+
+  for (var i = 0; i < songRows.length; i ++) {
+    songRows[i].addEventListener('mouseleave', function() {
+      this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+    })
+  }
 
 
-//
+/*
+// below is OK but not prefered; it's better to just use   "this" instead of songRows[i]
+// another solution is to use let i instead of var i   to scope i to the block
+// otherwise i will be pointing to the last value of i w/c is songRows.length or 5
+  for (var i = 0; i < songRows.length; i++) {
+     (function row(i) {
+       songRows[i].addEventListener('mouseleave', function(event) {
+          //this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+          songRows[i].children[0].innerHTML = songRows[i].children[0].getAttribute('data-song-number');
+          console.log('this = ', this);
+          console.log('this = ', songRows[i]);
+          console.log(i);
+        });
+     })(i);
+   }
+*/
+
+
+/*
+// this works fine
+// alternate method
+  Array.from(songRows).forEach(function(row) {
+    //console.log(row);
+    row.addEventListener('mouseleave', function(e) {
+      // console.log(e.target);
+      // console.log('this = ', this);
+      // console.log('row = ',  row);
+      // console.log(this === row); // true
+      // this.children[0].innerHTML = this.children[0].getAttribute('data-song-number'); // OK
+      row.children[0].innerHTML = row.children[0].getAttribute('data-song-number');
+
+    })
+  })
+*/
+
+}
