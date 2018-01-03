4037de298d6da15bbe723dc9f9567a3fda01f6af
Joseph <jvg_0119@yahoo.com>
2018-01-02 16:18:44 -0800

    cp 29 jq landing page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,9 @@
 
+// refactor animatePoints
+/*
 var pointsArray = document.getElementsByClassName('point'); // changed var points to pointsArray
-  var animatePoints = function(points) {
+//var pointsArray = $('.point');
+var animatePoints = function(points) {
 // var animatePoints = function() {
 //   var points = document.getElementsByClassName('point');
 
@@ -14,49 +17,182 @@ var pointsArray = document.getElementsByClassName('point'); // changed var point
   for (var i = 0; i < points.length; i++) {
     revealPoint(i);
   }
+
 }
 // animatePoints();
 // setTimeout(animatePoints, 1000);
+*/
+
+////////////////////
+// var pointsArray = $('point');
+
+var animatePoints = function() {
+  // console.log('pointsArray function running!!!');
+  // console.log('this in animatePoints = ',  $(this)); // this   is in the context of the window
+
+  var revealPoint = function() {
+    // console.log('this in revealPoint = ', $(this)); // this    is in the context of the revealPoint
+    // $(this).css('opacity', 1);
+    // $(this).css('transform', 'scaleX(1) translateY(0)')
+    $(this).css({'opacity': 1, 'transform' : 'scaleX(1) translateY(0)'}); // shorter form
+    // console.log(this.innerText)
+  }
+
+  // revealPoint();
+  // var $point = $('.point');
+  // $.each($('.point'), revealPoint);
+  $.each($('.point'), revealPoint);
+
+}
 
 
 ////////////////////
+//window.onload = function() {
+$(window).load(function() {
 
-window.onload = function() {
-
-  var sellingPoints = document.getElementsByClassName('selling-points')[0]; // first and only element in the nodelist
-  // console.log(sellingPoints); //
-
+  // refactor to jQuery
   // add this   to be able to trigger animatePoints if the screen is tall enough that it does not need to scroll
   // otherwise it will not animate
-  if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
+  // if (window.innerHeight > 950) {
+  //   animatePoints(pointsArray);
+  // }
+  if ($(window).height() > 950) {
+    animatePoints();
   }
 
-  // console.log(sellingPoints.getBoundingClientRect())
-  // Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-  //console.log('sellingPoints.getBoundingClientRect().top = ' +  sellingPoints.getBoundingClientRect().top)
-  window.addEventListener('scroll', function(event) {
+  // refactor to jQuery
+  // var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  // var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 400;
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-    console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
-    // Current offset or distance from the window top to the top of the sellingPoints
-
-    // console.log('window.innerHeight = ' +  window.innerHeight); // changes reference to the window size
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 400; // you can adjust this number to adjust the animatePoints trigger position; it's not very consistent w/ different screen sizes
-    console.log('scrollDistance = ' + scrollDistance);
-    //console.log(document.documentElement.scrollTop);
-    //console.log(document.body.scrollTop);
-    //var scrollDista23nce = sellingPoints.getBoundingClientRect().top - window.innerHeight;
-    //console.log(scrollDistance);
-    // if (sellingPoints.getBoundingClientRect().top < 400) {
-    //console.log('document.documentElement.scrollTop = ' +  document.documentElement.scrollTop);
-    //console.log('document.body.scrollTop = ' +  document.documentElement.scrollTop);
-
-    if (document.documentElement.scrollTop >= scrollDistance || document.body.scrollTop >= scrollDistance) {
-      console.log(scrollDistance, document.documentElement.scrollTop); // this will show when their equal
-      animatePoints(pointsArray);                                     // this is when it triggers the animatePoints
+  //refactor to jQuery
+  // window.addEventListener('scroll', function(event) {
+  // if (document.documentElement.scrollTop >= scrollDistance || document.body.scrollTop >= scrollDistance) {
+  //   console.log(scrollDistance, document.documentElement.scrollTop); // this will show when their equal
+  //   animatePoints(pointsArray);                                     // this is when it triggers the animatePoints
+  // }
+  $(window).scroll(function(event) {
+    // console.log('testing scroll!!!')
+    // console.log('bhmmm =',  $(window) === document.documentElement)
+    // console.log('window = ', $(window));
+    // console.log('document.documentElement = ', document)
+    // console.log($(window).scrollTop() === document.documentElement.scrollTop )
+    // console.log('scrollTop() = ', $(window).scrollTop(), ' scrollDistance = ', scrollDistance)
+    //if (document.documentElement.scrollTop >= scrollDistance || document.body.scrollTop >= scrollDistance) {
+    if ($(window).scrollTop() >= scrollDistance) { // scrollTop returns vertical scrollbar position
+    //   //console.log(scrollDistance, + ' : ' + document.documentElement.scrollTop()); // this will show when their equal
+    //  animatePoints(pointsArray);                                     // this is when it triggers the animatePoints
+      animatePoints();
+      //console.log('boooooahhh!!! @@@')
     }
+  });
+});
 
-  }); /* window.addEventListener */
+
+  // $(window).scroll(function(event) {
+  //   console.log('test = ');
+  // });
+    // if (document.documentElement.scrollTop >= scrollDistance || document.body.scrollTop >= scrollDistance) {
+    //   console.log(scrollDistance, document.documentElement.scrollTop); // this will show when their equal
+    //   animatePoints(pointsArray);                                     // this is when it triggers the animatePoints
+    // }
+    //console.log('test boooo' + event.target);
+    //console.log(document.documentElement)
+    //console.log($(document.documentElement.scrollTop >= scrollDistance));
+    // if ($(document.documentElement.scrollTop >= scrollDistance)) { // scrollTop returns vertical scrollbar position
+//     if ($(document.documentElement.scrollTop >= scrollDistance)) {
+//       if ($(window).scrollTop() >= scrollDistance) {
+//         console.log('myboooo');
+//         //animatePoints();
+//     //if ($(document.documentElement.scrollTop >= scrollDistance)) {
+//
+//       };
+//
+//   //
+//   //}); /* window.addEventListener */
+//   }
+//   /* window.onload */
+// };
+
+ //  $(window).
+ //
+ //
+ //
+ // }); /* $(window).load(function() { }*/
 
 
- }  /* window.onload */
+
+
+
+
+
+
+
+
+
+
+/* below are just test exploration - delete later */
+/*
+ $(window).load(function() {
+   console.log('bbbbbbboooo');
+   console.log("testing one");
+   var arr = ['Joe', 'George', 'Booo']
+   var domElement = document.querySelectorAll('.point-title');
+   var $newDomElement = $(domElement);
+   // console.log('$newDomElement = ',  $newDomElement.html());
+   //console.log('domElement = ',  domElement.innerText)
+   for (var i = 0; i < domElement.length; i++ ) {
+     domElement[i].innerHTML = '<H2>Testing ' + arr[i] + '!</H2>'
+     domElement[i].style.color = 'yellow';
+   }
+
+   var $newDomElement2 = $('.point-title')
+   console.log($newDomElement2[0].innerHTML)
+   console.log($newDomElement2[0])
+   $.each($newDomElement2, function(index, value) {
+     // $newDomElement2[index].text() = 'zoooo'
+     console.log(index + ' : ' + value.innerText);
+     value.innerHTML = '<H2>Testing ' + arr[index] + '!</H2>';
+   })
+
+   // $.each([ 52, 97 ], function( index, value ) {
+   //   console.log( index + ": " + value );
+   // });
+
+
+ })
+*/
+
+/*
+var domElement = document.getElementsByClassName('point-title');
+var $newDomElement = $(domElement)
+var arr = ['one', 'two', 'three']
+console.log(domElement.length);
+console.log($newDomElement.length);
+$(window).load(function() {
+   console.log('testing again!!!');
+  for (var i = 0; i < domElement.length; i++) {
+    // console.log(i)
+    // domElement[i].innerHTML = '<h1>' + 'Testing ' + arr[i] + '</h1>'
+    // console.log($newDomElement[i].innerText);
+    $newDomElement[i].innerHTML = '<h1>' + 'Testing ' + arr[i] + '</h1>';
+    // $newDomElement[i].style.color = 'yellow';
+    $($newDomElement[i]).css("color","blue"); // same as above
+  }
+})
+*/
+
+/*
+$(window).load(function() {
+  var arr = ['one', 'two', 'three'];
+  // console.log('test')
+  // console.log($('.point-title').length);
+  $('.point-title').each(function(i,value) {
+    // console.log('testing jquery')
+    // console.log(i + ' : ' + value.innerText);
+    value.innerHTML = '<h1>' + 'jQuery Test ' + arr[i] +  '</h1>'
+    $(value).css({'color':'green','fontFamily': 'cursive'});
+  //  $(value).css('fontFamily', 'cursive');
+  })
+})
+*/
